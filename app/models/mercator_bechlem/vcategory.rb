ffb0f5da4b37de44d24c0b52104285f60519d95c
Stefan Haslinger <stefan.haslinger@mittenin.at>
2014-04-19 11:47:12 +0200

    bechem logic

--- app/models/mercator_bechlem/vcategory.rb
+++ app/models/mercator_bechlem/vcategory.rb
@@ -1,26 +1,27 @@
 module MercatorBechlem
-  class Base < ActiveRecord::Base
+  class Vcategory < Base
 
     self.table_name = "VCATEGORY"
     self.primary_key = "IDCATEGORY"
-
-    def parent_category_id
-      cat_id = self.IDCATEGORY.to_s
-      cat_id[0..(9 - ( cat_id.count("0") + 2 ) ) ]
-    end
 
     def self.top_category_id
       1452
     end
 
-    def self.by_category_id( category_id = self.top_category_id )
-      cat_id = category_id.to_s
-      cat_id = cat_id + "0" * (9 - cat_id.length )
+    def self.ivellio_top_categories
+      children(category_id: 145210000) + children(category_id: 145220000)
+    end
+
+    def parent_category_id
+      cat_id = self.IDCATEGORY.to_s
+      cat_id[0..(9 - (cat_id.count("0") + 2))]
+    end
+
+    def self.children(category_id: 1452)
+      cat_id = category_id.to_s.ljust(9,'0')
       self.find_by_sql [
-      "SELECT c.* FROM VCATEGORY c, ITEM2ITEM i2i
-      WHERE i2i.idItem = c.idCategory
-      AND i2i.idParItem = ?
-      ORDER by idCategory", cat_id.to_i
+      "SELECT c.* FROM VCATEGORY c, ITEM2ITEM i2i WHERE i2i.idItem = c.idCategory
+      AND i2i.idParItem = ? ORDER by idCategory", cat_id.to_i
       ]
     end
 
--- app/models/mercator_bechlem/vitem_supply.rb
+++ app/models/mercator_bechlem/vitem_supply.rb
@@ -7,10 +7,8 @@ module MercatorBechlem
     def self.for_category_id( brand, category_id )
       cat_id = category_id.to_s.gsub("0", "")
       self.find_by_sql [
-      "select * from VITEM_SUPPLY
-        where idCategory like ?
-        and brand=?
-        order by artnr", "#{cat_id}%", brand
+      "select * from VITEM_SUPPLY where idCategory like ?
+        and brand=? order by artnr", "#{cat_id}%", brand
       ]
     end
 
