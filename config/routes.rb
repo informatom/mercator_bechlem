6f3049051b6ac3cc60f5d5de76521850ed309f69
Stefan Haslinger <stefan.haslinger@mittenin.at>
2014-04-21 11:30:13 +0200

    icecat routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,5 @@
 MercatorBechlem::Engine.routes.draw do
+  get 'products/:printer' => 'consumables#products', :as => 'consumables_products'
+  get 'printer/:printerseries' => 'consumables#printer', :as => 'consumables_printer'
+  get 'category/:category_id' => 'consumables#category', :as => 'consumables_category'
 end
